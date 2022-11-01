141c313e7b5b672b7ffe36c01ddac24f7c7305be
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-01 14:03:00 -0700

    Created Functionalty for PUT requests + Refactor song_detail

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Song
@@ -17,12 +18,24 @@ def songs_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def song_detail(request, pk):
-    try:
-        song = Song.objects.get(pk=pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method =='GET':
+    #New shortcut code that does the same thing
         serializer = SongSerializer(song)
         return Response(serializer.data)
-    except Song.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method =='PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    #The below is the original code
+    # try:
+    #     song = Song.objects.get(pk=pk)
+    #     serializer = SongSerializer(song)
+    #     return Response(serializer.data)
+    # except Song.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
         
